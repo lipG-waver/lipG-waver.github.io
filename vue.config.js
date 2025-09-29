b54123ddd874bcce47b33616b6656b7b83431b9f
Yunlong Zhou <17340910041@fudan.edu.cn>
2025-09-29 11:07:01 +0800

    fix:update publicPath for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/personal-site/' : '/'
+  publicPath: '/'  // 改成根路径
 })
